66da85ebc443472afacef2ff4bc14afab8349989
Pedro Hernandez <hispafox@gmail.com>
2025-07-30 14:23:25 +0200

    first commit

---
+++ DevDemoSolution/DevDemoData/ContextoBaseDatos.cs
@@ -0,0 +1,14 @@
+﻿using DevDemoEntities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevDemoData
+{
+    public class ContextoBaseDatos : DbContext
+    {
+        public ContextoBaseDatos(DbContextOptions<ContextoBaseDatos> options) : base(options)
+        {
+        }
+
+        public DbSet<Tipo> Tipos { get; set; }
+    }
+}
